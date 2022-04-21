bf7b4fac87fecef52f9d895ccc78e2e2af1eb22f
muharib0410 <abagithub0410@gmail.com>
2022-04-21 23:58:43 +0500

    js changes

--- script.js
+++ script.js
@@ -7,9 +7,22 @@ sounds.forEach (sound =>{
     btn.innerText = sound
     
     btn.addEventListener('click', () => {
+        stopSongs()
+
+
         document.getElementById(sound).play()
     })
 
     document.getElementById('buttons').
     appendChild(btn)
 })
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById
+        sound()
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
