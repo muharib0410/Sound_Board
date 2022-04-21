e620f9b844d07b748094a16240f09e97521f25d2
muharib0410 <abagithub0410@gmail.com>
2022-04-21 23:54:16 +0500

    changes on js

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ sounds.forEach (sound =>{
     const btn = document.createElement('button')
     btn.classList.add ('btn')
 
-    btn.innerText = sound 
+    btn.innerText = sound
+    
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
 
     document.getElementById('buttons').
     appendChild(btn)
